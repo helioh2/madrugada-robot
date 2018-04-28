349fb7a0ceb57634eb18973735ec72b484480db5
Helio <heliohenrique3@gmail.com>
2018-04-28 10:53:48 -0300

    Fixing and improving python module

--- src/turtle.py
+++ src/turtle.py
@@ -3,23 +3,22 @@ import math
 
 #Look for all Bluetooth devices
 #the computer knows about.
-print "Searching for devices..."
-print ""
+print("Searching for devices...")
 #Create an array with all the MAC
 #addresses of the detected devices
 nearby_devices = bluetooth.discover_devices()
 #Run through all the devices found and list their name
 num = 0
-print "Select your device by entering its coresponding number..."
+print("Select your device by entering its coresponding number...")
 for i in nearby_devices:
 	num+=1
-	print num , ": " , bluetooth.lookup_name( i )
+	print(num , ": " , bluetooth.lookup_name( i ))
 
 #Allow the user to select their Arduino
 #bluetooth module. They must have paired
 #it before hand.
 selection = input("> ") - 1
-print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
+print("You have selected", bluetooth.lookup_name(nearby_devices[selection]))
 bd_addr = nearby_devices[selection]
 
 port = 1
@@ -35,30 +34,56 @@ class Turtle:
 
 	def __init__(self):
 		#Connect to the bluetooth device
-    	#and initialize the GUI
-        self.sock.connect((bd_addr, port))
+		#and initialize the GUI
+		self.sock.connect((bd_addr, port))
 
-    def forward(self, distance):
-    	float x, y;
-    	#Updating internal state
-		self.angle = (self.angle * math.pi) / 180;
-		self.x = distance * math.cos(self.angle);
-		self.y = distance * math.sin(self.angle);
+	def calcDxDy(self, distance, angle):
+		angle_rad = math.radians(angle)
+		dx = distance * math.cos(angle_rad)
+		dy = distance * math.sin(angle_rad)
+		return dx, dy
+
+	def forward(self, distance):
+
+		dx, dy = calcDxDy(distance, self.angle)
+
+		#Updating internal state
+		self.x += dx;
+		self.y += dy;
 
 		#Building command
-		data = "M " + str(self.x) + " " + str(self.y)
+		data = "M " + str(dx) + " " + str(dy)
+		print(data)
+
+		#Sending command
+		self.sock.send(data)
+
+	def backward(self, distance):
+
+		inverted_angle = (self.angle + 180) % 360
+		dx, dy = calcDxDy(distance, inverted_angle)
+
+		#Updating internal state
+		self.x += dx;
+		self.y += dy;
+
+		#Building command
+		data = "M " + str(dx) + " " + str(dy)
+		print(data)
 
 		#Sending command
 		self.sock.send(data)
 
 	def turn(self, angle):
 		self.angle = (self.angle + angle) % 360
+		print(self.angle)
 
-	def pen_up(self):
-		self.pen_is_down = False
+		def pen_up(self):
+			self.pen_is_down = False
 
 		#Building command
 		data = "U"
+		print(data)
 
 		#Sending command
 		self.sock.send(data)
@@ -68,6 +93,7 @@ class Turtle:
 
 		#Building command
 		data = "D"
+		print(data)
 
 		#Sending command
 		self.sock.send(data)
@@ -78,13 +104,21 @@ class Turtle:
 
 		#Building command
 		data = "G " + str(self.x) + " " + str(self.y)
+		print(data)
 
 		#Sending command
 		self.sock.send(data)
 
 
 
-
+#tests:
+turtle = Turtle()
+turtle.pen_down()
+turtle.forward(50)
+turtle.turn(90)
+turtle.forward(50)
+turtle.pen_up()
+turtle.go_to(0,0)
 
 
 
